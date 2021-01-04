6574a7e915b3c232cc0c85e4f9ad32edfc24f2ac
Michael Squires <michaelsquires@hotmail.co.uk>
2021-01-04 16:55:32 +0000

    update scripts for migrate-make

---
+++ db/migrations/20210104164728_create_table_users_table.js
@@ -0,0 +1,50 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable("users", (usersTable) => {
+        usersTable.increments("userID").primary();
+        usersTable.text("username").notNullable;
+        usersTable.text("first_name").notNullable;
+        usersTable.text("last_name").notNullable;
+        usersTable.float("latitude").notNullable;
+        usersTable.float("longitude").notNullable;
+        usersTable.integer("date_of_birth").notNullable;
+        usersTable.text("gender")
+        usersTable.text("ability").notNullable;
+        usersTable.text("playing_hand").notNullable;
+        usersTable.text("club_membership").notNullable;
+        usersTable.boolean("mon_morn").notNullable;
+        usersTable.boolean("mon_aft").notNullable;
+        usersTable.boolean("mon_eve").notNullable;
+        usersTable.boolean("tues_morn").notNullable;
+        usersTable.boolean("tues_aft").notNullable;
+        usersTable.boolean("tues_eve").notNullable;
+        usersTable.boolean("wed_morn").notNullable;
+        usersTable.boolean("wed_aft").notNullable;
+        usersTable.boolean("wed_eve").notNullable;
+        usersTable.boolean("thurs_morn").notNullable;
+        usersTable.boolean("thurs_aft").notNullable;
+        usersTable.boolean("thurs_eve").notNullable;
+        usersTable.boolean("fri_morn").notNullable;
+        usersTable.boolean("fri_aft").notNullable;
+        usersTable.boolean("fri_eve").notNullable;
+        usersTable.boolean("sat_morn").notNullable;
+        usersTable.boolean("sat_aft").notNullable;
+        usersTable.boolean("sat_eve").notNullable;
+        usersTable.boolean("sun_morn").notNullable;
+        usersTable.boolean("sun_aft").notNullable;
+        usersTable.boolean("sun_eve").notNullable;
+        usersTable.text("description")
+        usersTable.text("photo").notNullable;
+        usersTable.integer("distance").default(10);
+        usersTable.text("min_ability")
+        usersTable.text("max_ability")
+        usersTable.text("hand_preference")
+        usersTable.integer("min_age")
+        usersTable.integer("max_age")
+        usersTable.text("gender_preference")
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("users");
+};
--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS tennis_matcher;
+DROP DATABASE IF EXISTS tennis_matcher_test;
 DROP DATABASE IF EXISTS tennis_matcher;
 
-CREATE DATABASE tennis_matcher_test;
+CREATE DATABASE tennis_matcher;
 CREATE DATABASE tennis_matcher_test;
