d94f1a1d70421ce37b423e8ecb92380931c55c1a
karben86 <benjaminkarst86@gmail.com>
2021-01-08 16:24:11 +0000

    adds cors

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const usersRouter = require("./routers/usersRouter")
 const clubsRouter = require("./routers/clubsRouter")
-//const cors = require('cors');
+const cors = require('cors');
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 app.use("/users", usersRouter)
 //app.use("/clubs", clubsRouter)
